f26b306c28d5fb3aa23998b17bcf17e6ac611ab2
easy4docker <easy4docker@gmail.com>
2021-09-16 22:34:39 -0700

    update

--- service-app/src/modules/Body.js
+++ service-app/src/modules/Body.js
@@ -1,13 +1,26 @@
-import React , { useEffect } from 'react';
+import React , { useEffect, useState } from 'react';
 import { Container } from 'react-bootstrap';
+import BarcodeScannerComponent from "./mobileComps/BarcodeScannerComponent.js";
 
 function Body(props) {
+  const [ data, setData ] = React.useState('Not Found');
+
   useEffect(() => {
   }, []);
 
   return (
     <Container fluid={true} className="m-0 p-0">
-       body
+      <span>yyy
+      <BarcodeScannerComponent
+        width={500}
+        height={500}
+        onUpdate={(err, result) => {
+          if (result) setData(result.text)
+          else setData('Not Found')
+        }}
+      />
+      <p>{data}</p>
+    </span>
     </Container>
    
   );
