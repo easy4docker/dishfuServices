3e9526b26af18c6fb150f23177d1c2725f631d2e
easy4docker <easy4docker@gmail.com>
2021-09-17 07:22:41 -0700

    update

--- service-app/src/modules/mobileComps/mobileScanner.js
+++ service-app/src/modules/mobileComps/mobileScanner.js
@@ -22,10 +22,10 @@ function MobileScanner(props) {
     setScanStatus(scanStatus ? false : true);
   }
   return (<Container fluid={true} className="p-1 text-center">
-      <Container fluid={true} className="p-1">
+      <Container fluid={true} className="p-1 text-center">
       {!scanStatus ? '' : (<BarcodeScannerComponent
-        width={'400'}
-        height={'400'}
+        width={'100%'}
+        height={'80%'}
         onUpdate={(err, result) => {
           if (result) setData(result.text)
           else setData('')
